69b25e5513c3d40614fc7cd57e1424f48918049f
Gayan <128354648+DMGCD@users.noreply.github.com>
2023-11-11 15:13:35 +0530

    For Dev branch

---
+++ src/controller/TimeFormcontroller.java
@@ -0,0 +1,54 @@
+package controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeFormcontroller {
+
+    public Label lblTime;
+    public Label lblTime12;
+    public Label lblDate;
+    public Button btn24H;
+    public Button btn12h;
+
+    public void initialize(){
+        lblTime.setVisible(false);
+        btn24H.setVisible(true);
+        btn12h.setVisible(false);
+
+        running12();
+}
+
+private void running12(){
+    final  Thread thread = new Thread(()->{
+
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss a");
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("EEE , MMM d , yyyy");
+        while(true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            final String time =simpleDateFormat.format(new Date());
+            final String date =simpleDateFormat1.format(new Date());
+            Platform.runLater(()->{
+                lblTime12.setText(time);
+                lblDate.setText(date);
+            });
+
+        }
+    });
+    thread.start();
+
+
+}
+
+
+}
